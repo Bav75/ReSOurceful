da2ef34cdd907f7a0f00942d7321b22bbd1fb3eb
Brian Velez <bvez94@gmail.com>
2019-08-07 14:18:22 -0700

    Created Sessions controller and associated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   ###static routes
-
-  #homepage 
+    #homepage 
   get '/', to: 'static#home' 
+
+  ###sessions routes 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   ###app resources 
   resources :users
