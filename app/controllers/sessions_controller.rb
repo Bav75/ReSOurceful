9c37dd54968dc50054d242e5f4b4215c0f2b8e53
Brian Velez <bvez94@gmail.com>
2019-08-07 14:39:35 -0700

    implemented basic log in functionality in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
     end
 
     def create
+        # binding.pry
+        @user = User.find_by(email: params[:session][:email])
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:alert] = "Invalid email and/or password provided."
+            redirect_to login_path
+        end
     end
 
     def destroy
