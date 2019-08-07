46d7d1bd381629b0a50349bfd953c77030bcfa7e
Brian Velez <bvez94@gmail.com>
2019-08-07 11:20:18 -0700

    Created Static controller. Created homepage route mapped to static controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  ###static routes
+
+  #homepage 
+  get '/', to: 'static#home' 
+
+  ###app resources 
   resources :users
+
 end
