60694a15558af4386638ed96e17298b6cb61f78d
Brian Velez <bvez94@gmail.com>
2019-08-09 12:20:11 -0700

    added checks on API response in searches#create action

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -15,23 +15,28 @@ require 'httparty'
         response = HTTParty.get(url)
         # binding.pry 
 
-        # create 5 search & answer objects 
-        5.times do |i|
-            answer = Answer.create(
-                title: response['items'][i]['title'],
-                post_link: response['items'][i]['link'],
-                accepted_answer_id: response['items'][i]['accepted_answer_id'],
-            )
-            search = current_user.searches.build(
-                answer_id: answer.id,
-                tags: tags 
-            )
-            search.save
-            # binding.pry
+        if response['items'].empty?
+            flash[:alert] = "No answers were found for the search tags provided. Please try a different combination of search tags."
+            render :new
+        else
+            # create 5 search & answer objects 
+            5.times do |i|
+                answer = Answer.create(
+                    title: response['items'][i]['title'],
+                    post_link: response['items'][i]['link'],
+                    accepted_answer_id: response['items'][i]['accepted_answer_id'],
+                )
+                search = current_user.searches.build(
+                    answer_id: answer.id,
+                    tags: tags 
+                )
+                search.save
+                # binding.pry
+            end
+            binding.pry
+            # use request.referer
+            redirect_to user_searches_path
         end
-        binding.pry
-        # use request.referer
-        redirect_to user_searches_path
     end
 
     # will need to build both searches and associated answers within this create method 
