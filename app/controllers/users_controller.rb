81d6c52cec7ac29b4e68b2911e7e0a340f14cb1f
Brian Velez <bvez94@gmail.com>
2019-08-11 16:18:50 -0700

    refactored user signup in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,24 @@ class UsersController < ApplicationController
    end 
 
    def create
-    @user = User.find_or_create_by(user_params)
-
-    if @user.persisted?
-        log_in(@user)
-        redirect_to user_path(@user)
-    else
-        #if user didn't persist - load errors to flash msg & redirect 
-        @user.errors.messages.each do |error_symbol, message|
-            flash[error_symbol] = "There appears to be an error: #{error_symbol} #{message[0]}"
-        end
+    # binding.pry
+    
+    if User.find_by(username: params[:user][:username])
+        flash[alert] = "This username already exists. Please try a different username."
         redirect_to new_user_path
+    else
+        @user = User.create(user_params)
+
+        if @user.persisted?
+            log_in(@user)
+            redirect_to user_path(@user)
+        else
+            #if user didn't persist - load errors to flash msg & redirect 
+            @user.errors.messages.each do |error_symbol, message|
+                flash[error_symbol] = "There appears to be an error: #{error_symbol} #{message[0]}"
+            end
+            redirect_to new_user_path
+        end
     end
 
    end
